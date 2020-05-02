232814d953a21e2744f3bd5e346c141726912cf5
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-02 18:21:28 +0530

    Add title and icons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,10 +7,61 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+
+  ];
+
+  Widget _buildIcon(int index){
+    return Container(
+      height: 60.0,
+      width: 60.0,
+      decoration: BoxDecoration(
+        color: Theme.of(context).accentColor,
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Icon(
+        _icons[index],
+        size: 25.0,
+        color: Theme.of(context).primaryColor,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical:30),
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left:20.0, right:120.0),
+              child: Text(
+                'What would you like to find ?',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children:  _icons
+                  .asMap()
+                  .entries
+                  .map(
+                    (MapEntry map) => _buildIcon(map.key),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
