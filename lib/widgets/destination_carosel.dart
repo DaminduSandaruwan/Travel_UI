db250380b3145f00516f50bce6d58fab815eb978
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-03 22:06:07 +0530

    Add images

--- lib/widgets/destination_carosel.dart
+++ lib/widgets/destination_carosel.dart
@@ -49,38 +49,68 @@ class DestinationCarosel extends StatelessWidget {
                 width: 210.0,
                 color: Colors.red,
                 child: Stack(
+                  alignment: Alignment.topCenter,
                   children: <Widget>[
+                    Positioned(
+                      bottom: 15.0,
+                      child: Container(
+                        height: 120.0,
+                        width: 200.0,                      
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end ,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                '${destination.activities.length} Activities',
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: 1.2,
+                                ),
+                              ),
+                              Text(
+                                destination.description,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  
+                                ),
+                              ),
+                               
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                     Container(
-                      height: 120.0,
-                      width: 200.0,                      
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26, 
+                            offset: Offset(0.0,2.0),
+                            blurRadius: 6.0
+                            )
+                        ]
                       ),
-                      child: Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end ,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              '${destination.activities.length} Activities',
-                              style: TextStyle(
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.w600,
-                                letterSpacing: 1.2,
-                              ),
+                      child: Stack(
+                        children: <Widget>[
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image(
+                              height: 180.0,
+                              width: 180.0,
+                              image: AssetImage(destination.imageUrl),
+                              fit: BoxFit.cover,
                             ),
-                            Text(
-                              destination.description,
-                              style: TextStyle(
-                                color: Colors.grey,
-                                
-                              ),
-                            ),
-                             
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
