a6be2fbd30f2eb752ab76570dae00428bf91bee9
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-04 18:33:07 +0530

    add city and country

--- lib/widgets/destination_carosel.dart
+++ lib/widgets/destination_carosel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_place/models/destination_model.dart';
 
 class DestinationCarosel extends StatelessWidget {
@@ -108,6 +109,36 @@ class DestinationCarosel extends StatelessWidget {
                               width: 180.0,
                               image: AssetImage(destination.imageUrl),
                               fit: BoxFit.cover,
+                            ),
+                          ),
+                          Positioned(
+                            left: 10.0,
+                            bottom: 10.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  destination.city, 
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1.2,
+                                  ),                               
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Icon(FontAwesomeIcons.locationArrow,size: 10.0,color: Colors.white,),
+                                    SizedBox(width: 5.0,),
+                                    Text(
+                                      destination.country,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
                         ],
